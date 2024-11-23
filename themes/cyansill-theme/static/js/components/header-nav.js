faff7eccb37ec97fa3f1bfe268aa9cdd423231ba
cyansill <sill_dimitrios@icloud.com>
2024-11-24 01:12:41 +0800

    solve problem display wrong when visit on mobile device

--- themes/cyansill-theme/static/js/components/header-nav.js
+++ themes/cyansill-theme/static/js/components/header-nav.js
@@ -20,21 +20,36 @@
       break
   }
   var header_html = `
-<ul class="layui-nav">
-  <li class="layui-nav-item ${ layui_this === 0 ? 'layui-this' : '' }">
+<ul class="layui-nav dynamic-nav">
+  <li class="layui-nav-item dynamic-nav-item ${ layui_this === 0 ? 'layui-this' : '' }">
     <a href="/">主页</a>
   </li>
-  <li class="layui-nav-item ${ layui_this === 1 ? 'layui-this' : '' }">
+  <li class="layui-nav-item dynamic-nav-item ${ layui_this === 1 ? 'layui-this' : '' }">
     <a href="/categories">分类</a>
   </li>
-  <li class="layui-nav-item ${ layui_this === 2 ? 'layui-this' : '' }">
+  <li class="layui-nav-item dynamic-nav-item ${ layui_this === 2 ? 'layui-this' : '' }">
     <a href="/tags">标签</a>
   </li>
-  <li class="layui-nav-item ${ layui_this === 3 ? 'layui-this' : '' }">
+  <li class="layui-nav-item dynamic-nav-item ${ layui_this === 3 ? 'layui-this' : '' }">
     <a href="/info/about">关于</a>
   </li>
 </ul>
   `
 
   $('#header-nav') ? $('#header-nav').html(header_html) : null
+  function dynamicStyle() {
+    if ($(window).width() < 768) {
+      $('.dynamic-nav').css('display', 'flex')
+      $('.dynamic-nav-item').css('flex', '1 1 auto')
+      $('.dynamic-nav-item').css('text-align', 'center')
+    } else {
+      $('.dynamic-nav').css('display', 'block')
+      $('.dynamic-nav-item').css('flex', 'none')
+      $('.dynamic-nav-item').css('text-align', 'center')
+    }
+  }
+  $(window).on('resize', function () {
+    dynamicStyle()
+  })
+  dynamicStyle()
 })()
